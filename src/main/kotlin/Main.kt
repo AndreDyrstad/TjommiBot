c52354309448b5fcbaa315c026a4e6a784f6c316
AdyBouvet <andre.dyrstad@bouvet.no>
2021-11-26 15:16:57 +0100

    Added env variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,12 @@ import discord4j.core.DiscordClient
 import discord4j.core.GatewayDiscordClient
 import discord4j.core.`object`.entity.channel.MessageChannel
 import discord4j.core.event.domain.message.MessageCreateEvent
+import io.github.cdimascio.dotenv.Dotenv
 import reactor.core.publisher.Mono
 
 fun main() {
-    DiscordClient.create("")
+    val dotenv = Dotenv.load()
+    DiscordClient.create(dotenv.get("TOKEN"))
         .withGateway { client: GatewayDiscordClient ->
             client.on(MessageCreateEvent::class.java) { event: MessageCreateEvent ->
                 val message = event.message
